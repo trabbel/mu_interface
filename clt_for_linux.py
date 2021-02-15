a220fc60dd13c485de2c6f1cdbdc965f34da344c
Till.Aust <till.aust@student.uib.no>
2021-02-15 15:22:35 +0100

    now you dont need crtl c

--- clt_for_linux.py
+++ clt_for_linux.py
@@ -7,6 +7,7 @@ ser = serial.Serial(
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS
+    #exclusive = None
 )
 
 # 1. start MU
@@ -18,34 +19,37 @@ ser = serial.Serial(
 
 # (to restart any application you have to turn everything of and start over again!)
 
-try:
-    while 1:
-        print(ser.read(1))
-
-except KeyboardInterrupt:
+ser.reset_input_buffer()
+ser.reset_output_buffer()
+while 1:
+    print("try to open port...")
+    #time.sleep(1)
+    if ser.inWaiting() > 0:
+        inp = "mp*"
+        ser.write([ord(e) for e in inp])
+        break
+    
+    #ser.close()
+    #time.sleep(1)
+    #ser.open()
+    #time.sleep(1)
+        
     # reset buffers -> idk if necessary?
-    ser.reset_input_buffer()
-    ser.reset_output_buffer()
+    #ser.reset_input_buffer()
+    #ser.reset_output_buffer()
 
-    inp = "mp*"  # stop measuring command see user maual
-    ser.write([ord(e) for e in inp])
+# normal control loop
+print('Enter your commands below.\r\nInsert "exit" to leave the application.')
+while 1:
+    inp = input(">> ")
+    if inp == 'exit':
+        ser.close()
+        exit()
+    else:
+        ser.write([ord(e) for e in inp])
+        out = []
+        time.sleep(1)
+        while ser.inWaiting() > 0:
+            out.append(ser.read(1))
 
-    # reset buffers -> idk if necessary?
-    ser.reset_input_buffer()
-    ser.reset_output_buffer()
-
-    # normal control loop
-    print('Enter your commands below.\r\nInsert "exit" to leave the application.')
-    while 1:
-        inp = input(">> ")
-        if inp == 'exit':
-            ser.close()
-            exit()
-        else:
-            ser.write([ord(e) for e in inp])
-            out = []
-            time.sleep(1)
-            while ser.inWaiting() > 0:
-                out.append(ser.read(1))
-
-            print(">> " + str(b''.join(out).decode('ascii')))
+        print(">> " + str(b''.join(out).decode('ascii')))
