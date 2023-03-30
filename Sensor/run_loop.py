6c12efe55fcada94ca5adff94b31da2aff5c496c
Tim-Lucas Rabbel <45310810+trabbel@users.noreply.github.com>
2023-03-30 13:11:34 +0200

    Add address field to run_loop

--- Sensor/run_loop.py
+++ Sensor/run_loop.py
@@ -2,8 +2,9 @@ from subprocess import Popen
 import sys
 
 port = sys.argv[1]
+addr = sys.argv[2]
 filename = "main.py"
 while True:
     print("\nStarting " + filename)
-    p = Popen(f"python3 {filename} --port {port} --int 1000", shell=True)
+    p = Popen(f"python3 {filename} --port {port} --int 1000 --addr {addr}", shell=True)
     p.wait()
