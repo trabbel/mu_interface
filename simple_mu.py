1b50de024cc691f152ed812d75a1eac26493bb88
Marko Krizmancic <marko.krizmancic@fer.hr>
2023-04-11 11:50:39 +0200

    Add reset to simple script

--- simple_mu.py
+++ simple_mu.py
@@ -2,7 +2,7 @@ import serial
 import time
 
 PORT = '/dev/ttyACM0'
-BAUDRATE = 115200
+BAUDRATE = 460800
 INT = 1000
 
 # Initialize serial port.
@@ -15,23 +15,19 @@ ser = serial.Serial(
     timeout=1,
     xonxoff=False,
     rtscts=True,
-    dsrdtr=False,
-    exclusive=True
+    dsrdtr=False
 )
 
 # Open the port.
 ser.port = PORT
+ser.rts = True
+ser.dtr = False
 time.sleep(0.1)
 ser.open()
 time.sleep(0.1)
-ser.rts = False
-ser.dtr = False
-# ser.close()
-# time.sleep(0.1)
-# ser.open
 
-# ser.reset_input_buffer()
-# ser.reset_output_buffer()
+ser.reset_input_buffer()
+ser.reset_output_buffer()
 time.sleep(0.1)
 print("Port opened.")
 
@@ -41,7 +37,6 @@ print("Starting measurements...")
 # Set measurement interval.
 set_interval = ',mi{:05}*'.format(INT)
 ser.write(set_interval.encode())
-time.sleep(1.0)
 
 # Start measurements.
 ser.write(b',ms*')
@@ -57,6 +52,8 @@ time.sleep(1.0)
 # Stop measurements.
 print("Stopping measurements...")
 ser.write(b',mp*')
+time.sleep(0.1)
+ser.write(b',sr*')
 time.sleep(1.0)
 
 # Close the port.
