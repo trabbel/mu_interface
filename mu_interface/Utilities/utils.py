c5cd8e1f307cb26a3620f3b0388af8dc0ed688ba
mkrizmancic <marko.krizmancic@fer.hr>
2023-05-04 11:17:47 +0200

    Bring back old ip for windows

--- mu_interface/Utilities/utils.py
+++ mu_interface/Utilities/utils.py
@@ -1,5 +1,6 @@
-from subprocess import check_output
+import socket
 
 def get_ip_address():
-    ip = check_output(['hostname', '-I']).decode().strip()
-    return ip if ip else '127.0.0.1'
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
