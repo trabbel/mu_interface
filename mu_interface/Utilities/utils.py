a5e4da2d41a61a3849291f294578a69dc698b837
mkrizmancic <marko.krizmancic@fer.hr>
2023-05-04 15:10:56 +0200

    Return default IP when no wifi

--- mu_interface/Utilities/utils.py
+++ mu_interface/Utilities/utils.py
@@ -1,6 +1,9 @@
 import socket
 
 def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        return s.getsockname()[0]
+    except OSError:
+        return "127.0.0.1"
